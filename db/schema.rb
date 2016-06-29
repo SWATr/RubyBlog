8b487abc61d35c1c6c9a71d7e38f0568e8d2cd7e
NateCZimmerman <kingmoddie98@gmail.com>
2016-06-28 21:41:31 -0400

    Users added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629011614) do
+ActiveRecord::Schema.define(version: 20160629014052) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string   "post_title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160629011614) do
     t.string   "post_image_url"
     t.string   "post_author"
     t.datetime "post_time"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_diget"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
