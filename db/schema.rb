8d85b4d16bb5c45fa2af0e20fc006b9060be0b5d
NateCZimmerman <kingmoddie98@gmail.com>
2016-06-29 20:43:12 -0400

    DB for users fixed and examples added

--- db/schema.rb
+++ db/schema.rb
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(version: 20160629014052) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "password_diget"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
