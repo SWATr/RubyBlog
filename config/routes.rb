a83a06d435b2b5e6b573c3d6995afa7ba5ebdb50
Jon <hp007hp31@gmail.com>
2016-06-29 22:35:46 -0400

    Bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'blog/index'
   
   #get 'blog_posts/new'
-=======
- get 'admin' => 'admin#index'
+  get 'admin' => 'admin#index'
  
- controller :sessions do
-   get 'login' => :new
-   post 'login' => :create
-   delete 'logout' => :destroy
- end
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 
-
->>>>>>> 20782a70c2721567d7f83065902ac699677abe30
   resources :users
   resources :blog_post
 
