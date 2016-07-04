5a8218cba340fa6aa217dd1bbecc094e048a9ad2
NateCZimmerman <kingmoddie98@gmail.com>
2016-07-03 22:52:43 -0400

    start for user sort

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,6 +1,8 @@
 class BlogPost < ActiveRecord::Base
 
   #attr_accessible :description, :image_url, :title
+  belongs_to :user
+
   validates :blog_title, :description, :image_url, presence: true
   validates :blog_title, uniqueness: true
 end
