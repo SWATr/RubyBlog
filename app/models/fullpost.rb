58394d1af0a584b3de70ad71ccf94687ba064059
NateCZimmerman <kingmoddie98@gmail.com>
2016-07-07 00:07:47 -0400

    Here i got time to blow up the database

--- app/models/fullpost.rb
+++ app/models/fullpost.rb
@@ -1,7 +1,4 @@
 class Fullpost < ActiveRecord::Base
 validates_presence_of :body, :title, :author
-#validates_acceptance_of :tag           
-#                       :accept => "Food"
-						
-
+validates :tag, acceptance: { accept: 'Food' }, acceptance: { accept: 'DIY' }, acceptance: { accept: 'Vidoes' }, acceptance: { accept: 'News' }
 end
