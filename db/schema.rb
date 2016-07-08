368e3a6be9af7421be46285239f563ece2af883a
Jon <hp007hp31@gmail.com>
2016-07-07 23:47:51 -0400

    Added admin flag groundworks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708002337) do
+ActiveRecord::Schema.define(version: 20160708034245) do
 
   create_table "blog_comments", force: :cascade do |t|
     t.integer  "blog_post_id"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20160708002337) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "admin"
   end
 
 end
